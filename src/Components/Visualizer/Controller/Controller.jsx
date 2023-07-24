1be6ef39f3e14059119c1e7a0cff5796db25b1ca
Alex Gunnarsson <alexgunnarsson@live.se>
2023-07-24 23:56:51 +0200

    work on dropdown menu

--- src/Components/Visualizer/Controller/Controller.jsx
+++ src/Components/Visualizer/Controller/Controller.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import "./Controller.css";
-import Dropdown from "./Dropdown/Dropdown";
+import { Dropdown, DropdownMenu, DropdownItem } from "./Dropdown/Dropdown";
 
 function Controller({ state, reloadGrid }) {
   return (
     <div className="controller">
       <div className="controls">
         <Dropdown>
+          <DropdownMenu>
+            <DropdownItem>kek</DropdownItem>
+            <DropdownItem>kek2</DropdownItem>
+            <DropdownItem>kek3</DropdownItem>
+            <DropdownItem>kek4</DropdownItem>
+          </DropdownMenu>
+          <DropdownMenu>
+            <DropdownItem>lmao</DropdownItem>
+            <DropdownItem>lmao2</DropdownItem>
+            <DropdownItem>lmao3</DropdownItem>
+            <DropdownItem>lmao4</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        {/* <Dropdown>
           <h1>loler</h1>
         </Dropdown>
         <Dropdown>
           <h1>loler</h1>
-        </Dropdown>
+        </Dropdown> */}
         <button
           onClick={() => {
             state.settings.paintWeight =
--- src/Components/Visualizer/Controller/Dropdown/Dropdown.jsx
+++ src/Components/Visualizer/Controller/Dropdown/Dropdown.jsx
@@ -1,15 +1,36 @@
 import React, { useState } from "react";
 import "./Dropdown.css";
 
-function Dropdown(props) {
+export function Dropdown(props) {
   const [open, setOpen] = useState(false);
+  const [activeMenu, setActiveMenu] = useState(0);
+
+  const menus = Array.isArray(props.children)
+    ? props.children
+    : [props.children];
+  // Pass on setState
+  menus.map((menu) => {
+    // console.log(menu);
+  });
 
   return (
     <>
-      <button onClick={() => setOpen((prev) => !prev)}>D</button>
-      {open && props.children}
+      <button onClick={() => setOpen((prev) => !prev)}>{props.icon}</button>
+      {open && menus}
     </>
   );
 }
 
-export default Dropdown;
+export function DropdownMenu(props) {
+  return <div className="dropdown-menu">{props.children}</div>;
+}
+
+export function DropdownItem(props) {
+  return (
+    <div className={"dropdown-item" + (props.clickable ? " clickable" : "")}>
+      <span className="dropdown-item-icon-left">{props.leftIcon}</span>
+      {props.children}
+      <span className="dropdown-item-icon-right">{props.rightIcon}</span>
+    </div>
+  );
+}
