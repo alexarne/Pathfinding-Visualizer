d0af07b88a57deee5b828f1f0719409bba806972
Alex Gunnarsson <alexgunnarsson@live.se>
2023-08-10 02:10:35 +0200

    refactor bfs and dfs

--- src/utils/pathfindingAlgorithms.js
+++ src/utils/pathfindingAlgorithms.js
@@ -75,25 +75,21 @@ function GBFS(grid, from, to) {
 
 function BFS(grid, from, to) {
   const visitedCellsInOrder = [];
-  const parent = Array(grid.length)
-    .fill(0)
-    .map((row, y) => {
-      return Array(grid[0].length).fill(-1);
-    });
+  const parent = getParentGrid(grid);
 
   const queue = [{ pos: from, parentPos: { x: -1, y: -1 } }];
   while (queue.length > 0) {
     const node = queue.shift();
     if (!valid(node, grid)) continue;
-    if (parent[node.pos.y][node.pos.x] !== -1) continue;
-
+    if (visited(node, parent)) continue;
     visitedCellsInOrder.push({ x: node.pos.x, y: node.pos.y });
-    parent[node.pos.y][node.pos.x] = {
-      x: node.parentPos.x,
-      y: node.parentPos.y,
-    };
+    setParentNode(parent, node);
     if (node.pos.x == to.x && node.pos.y == to.y) break;
-    pushNeighbours(queue, node);
+
+    const neighbours = getNeighbours(node);
+    for (const neighbour of neighbours) {
+      queue.push(neighbour);
+    }
   }
 
   const shortestPathInOrder = constructPath(to, parent);
@@ -102,25 +98,21 @@ function BFS(grid, from, to) {
 
 function DFS(grid, from, to) {
   const visitedCellsInOrder = [];
-  const parent = Array(grid.length)
-    .fill(0)
-    .map((row, y) => {
-      return Array(grid[0].length).fill(-1);
-    });
+  const parent = getParentGrid(grid);
 
   const stack = [{ pos: from, parentPos: { x: -1, y: -1 } }];
   while (stack.length > 0) {
     const node = stack.pop();
     if (!valid(node, grid)) continue;
-    if (parent[node.pos.y][node.pos.x] !== -1) continue;
-
+    if (visited(node, parent)) continue;
     visitedCellsInOrder.push({ x: node.pos.x, y: node.pos.y });
-    parent[node.pos.y][node.pos.x] = {
-      x: node.parentPos.x,
-      y: node.parentPos.y,
-    };
+    setParentNode(parent, node);
     if (node.pos.x == to.x && node.pos.y == to.y) break;
-    pushNeighbours(stack, node);
+
+    const neighbours = getNeighbours(node);
+    for (const neighbour of neighbours) {
+      stack.push(neighbour);
+    }
   }
 
   const shortestPathInOrder = constructPath(to, parent);
@@ -130,6 +122,14 @@ function DFS(grid, from, to) {
 /**
  * General helper functions
  */
+
+function getParentGrid(grid) {
+  return Array(grid.length)
+    .fill(0)
+    .map((row, y) => {
+      return Array(grid[0].length).fill(-1);
+    });
+}
 
 function valid(node, grid) {
   return (
@@ -141,23 +141,36 @@ function valid(node, grid) {
   );
 }
 
-function pushNeighbours(struct, node) {
-  struct.push({
-    pos: { x: node.pos.x + 1, y: node.pos.y },
-    parentPos: { x: node.pos.x, y: node.pos.y },
-  });
-  struct.push({
-    pos: { x: node.pos.x, y: node.pos.y + 1 },
-    parentPos: { x: node.pos.x, y: node.pos.y },
-  });
-  struct.push({
-    pos: { x: node.pos.x - 1, y: node.pos.y },
-    parentPos: { x: node.pos.x, y: node.pos.y },
-  });
-  struct.push({
-    pos: { x: node.pos.x, y: node.pos.y - 1 },
-    parentPos: { x: node.pos.x, y: node.pos.y },
-  });
+function visited(node, parent) {
+  return parent[node.pos.y][node.pos.x] !== -1;
+}
+
+function setParentNode(parent, node) {
+  parent[node.pos.y][node.pos.x] = {
+    x: node.parentPos.x,
+    y: node.parentPos.y,
+  };
+}
+
+function getNeighbours(node) {
+  return [
+    {
+      pos: { x: node.pos.x + 1, y: node.pos.y },
+      parentPos: { x: node.pos.x, y: node.pos.y },
+    },
+    {
+      pos: { x: node.pos.x, y: node.pos.y + 1 },
+      parentPos: { x: node.pos.x, y: node.pos.y },
+    },
+    {
+      pos: { x: node.pos.x - 1, y: node.pos.y },
+      parentPos: { x: node.pos.x, y: node.pos.y },
+    },
+    {
+      pos: { x: node.pos.x, y: node.pos.y - 1 },
+      parentPos: { x: node.pos.x, y: node.pos.y },
+    },
+  ];
 }
 
 function constructPath(end, parent) {
