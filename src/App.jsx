602aaff0ba1aa930a339065184d6ac214c7708d4
Alex Gunnarsson <alexgunnarsson@live.se>
2023-08-12 23:58:28 +0200

    test other fix to mobile keyboard shrinking grid on load

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
+import { ParamsProvider } from "./Components/Context";
+import Controller from "./Components/Controller/Controller";
 import Header from "./Components/Header/Header";
 import Visualizer from "./Components/Visualizer/Visualizer";
-import Controller from "./Components/Controller/Controller";
-import { ParamsProvider } from "./Components/Context";
-import { useEffect } from "react";
 
 function App() {
-  // Circumvent problem with keyboard changing
-  // the viewport size on mobile
-  useEffect(() => {
-    var w = Math.max(
-      document.documentElement.clientWidth,
-      window.innerWidth || 0
-    );
-    var h = Math.max(
-      document.documentElement.clientHeight,
-      window.innerHeight || 0
-    );
-    document.querySelector("html, body").style = `
-      width: ${w}px;
-      height: ${h}px;
-    `;
-  }, []);
-
   return (
     <ParamsProvider>
       <Header />
