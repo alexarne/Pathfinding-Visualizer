34bfcf0b4607eca35642a0e821ee8080c08d2000
Alex Gunnarsson <alexgunnarsson@live.se>
2023-08-13 23:16:14 +0200

    implement unweighted gbfs and A*

--- src/utils/pathfindingAlgorithms.js
+++ src/utils/pathfindingAlgorithms.js
@@ -1,3 +1,5 @@
+import { PriorityQueue } from "@datastructures-js/priority-queue";
+
 /**
  * The available algorithms, mapped to their respective functions,
  * and their respective shortened display names.
@@ -14,6 +16,7 @@ export const algorithmMappings = {
 export const getAbbreviation = {
   "Dijkstra's Algorithm": "Dijkstra",
   "A* Search": "A*",
+  "Greedy Best-First Search": "GBFS",
   "Breadth-First Search": "BFS",
   "Depth-First Search": "DFS",
 };
@@ -63,14 +66,69 @@ function Dijkstra(grid, from, to) {
 
 function AStar(grid, from, to) {
   const visitedCellsInOrder = [];
-  const shortestPathInOrder = [];
-  return ["astar", shortestPathInOrder];
+  const parent = getParentGrid(grid);
+  const pq = getPriorityQueue();
+
+  pq.enqueue({
+    pos: from,
+    parentPos: { x: -1, y: -1 },
+    currentDistance: 0,
+    heuristicValue: -1,
+  });
+  while (!pq.isEmpty()) {
+    const node = pq.dequeue();
+    console.log(node.pos);
+    if (!valid(node, grid)) continue;
+    if (visited(node, parent)) continue;
+    visitedCellsInOrder.push({ x: node.pos.x, y: node.pos.y });
+    setParentNode(parent, node);
+    if (node.pos.x == to.x && node.pos.y == to.y) break;
+
+    const neighbours = getNeighbours(node);
+    for (const neighbour of neighbours) {
+      const newDistance = node.currentDistance + 1;
+      pq.enqueue({
+        ...neighbour,
+        currentDistance: newDistance,
+        heuristicValue: newDistance + manhattanDistance(neighbour.pos, to),
+      });
+    }
+  }
+
+  const shortestPathInOrder = constructPath(to, parent);
+  return [visitedCellsInOrder, shortestPathInOrder];
 }
 
 function GBFS(grid, from, to) {
   const visitedCellsInOrder = [];
-  const shortestPathInOrder = [];
-  return ["gbfs", shortestPathInOrder];
+  const parent = getParentGrid(grid);
+  const pq = getPriorityQueue();
+
+  pq.enqueue({
+    pos: from,
+    parentPos: { x: -1, y: -1 },
+    heuristicValue: -1,
+  });
+  while (!pq.isEmpty()) {
+    const node = pq.dequeue();
+    console.log(node.pos);
+    if (!valid(node, grid)) continue;
+    if (visited(node, parent)) continue;
+    visitedCellsInOrder.push({ x: node.pos.x, y: node.pos.y });
+    setParentNode(parent, node);
+    if (node.pos.x == to.x && node.pos.y == to.y) break;
+
+    const neighbours = getNeighbours(node);
+    for (const neighbour of neighbours) {
+      pq.enqueue({
+        ...neighbour,
+        heuristicValue: manhattanDistance(neighbour.pos, to),
+      });
+    }
+  }
+
+  const shortestPathInOrder = constructPath(to, parent);
+  return [visitedCellsInOrder, shortestPathInOrder];
 }
 
 function BFS(grid, from, to) {
@@ -122,6 +180,19 @@ function DFS(grid, from, to) {
 /**
  * General helper functions
  */
+
+function getPriorityQueue() {
+  // Sorted in increasing order
+  const comparator = (a, b) => {
+    if (a.heuristicValue === b.heuristicValue) return 0;
+    return a.heuristicValue > b.heuristicValue ? 1 : -1;
+  };
+  return new PriorityQueue(comparator);
+}
+
+function manhattanDistance(from, to) {
+  return Math.abs(from.x - to.x) + Math.abs(from.y - to.y);
+}
 
 function getParentGrid(grid) {
   return Array(grid.length)
